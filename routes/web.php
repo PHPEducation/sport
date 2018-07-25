bd8ee8d9bc73ba4212dce9907d1a1eec1f2722c1
Cong Hoang <toilahtc@gmail.com>
2018-07-25 11:08:46 +0700

    add header, footer, master blade file
    	new file:   .bowerrc
    	modified:   .gitignore
    	modified:   app/Http/Controllers/Auth/LoginController.php
    	new file:   app/Http/Controllers/HomeController.php
    	new file:   app/Http/Controllers/Vendor/VendorsController.php
    	modified:   app/Http/Kernel.php
    	new file:   app/Http/Middleware/Vendor.php
    	modified:   app/Providers/AppServiceProvider.php
    	new file:   bower.json
    	modified:   package.json
    	new file:   resources/views/auth/login.blade.php
    	new file:   resources/views/auth/passwords/email.blade.php
    	new file:   resources/views/auth/passwords/reset.blade.php
    	new file:   resources/views/auth/register.blade.php
    	new file:   resources/views/auth/vendorLogin.blade.php
    	new file:   resources/views/layouts/app.blade.php
    	new file:   resources/views/master.blade.php
    	new file:   resources/views/shared/footer.blade.php
    	new file:   resources/views/shared/header.blade.php
    	modified:   resources/views/welcome.blade.php
    	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,26 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('users/login', [
+    'as' => 'login',
+    'uses' => 'Auth\LoginController@showLoginForm'
+]);
+/*Route::get('vendors/login', [
+    'as' => 'login',
+    'uses' => 'Auth\LoginController@showVendorLoginForm'
+]);*/
+
+Route::get('/products', 'ProductsController@index');
+
+Route::group(array('prefix' => 'vendor','namespace' => 'Vendor' , 'middleware' => 'vendor')
+    ,
+    function () {
+        Route::get('/','VendorsController@home');
+
+        Route::get('/login', "VendorsController@showLoginForm");
+
+        Route::get('/logout',"VendorsController@logout");
+    });
