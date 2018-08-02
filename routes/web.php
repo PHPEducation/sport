244f825487334d64f18d03e4a35ff3cee86d7fcd
Cong Hoang <toilahtc@gmail.com>
2018-08-02 13:29:09 +0700

    add controller Auth

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('getHome');
 
 Route::get('/product', 'Vendor\ProductController@newProduct')->name('newProduct');
 
@@ -23,10 +23,21 @@ Route::post('/product', 'Vendor\ProductController@store')->name('storeProduct');
 
 Auth::routes();
 
-Route::get('users/login', [
-    'as' => 'login',
-    'uses' => 'Auth\LoginController@showLoginForm'
-]);
+Route::get('/login', 'Auth\LoginController@getLogin');
+
+Route::post('/login', 'Auth\LoginController@postLogin');
+
+Route::post('/register', 'Auth\RegisterController@createUser');
+
+Route::get('/vendorLogin', 'Auth\LoginController@getLoginVendor');
+
+Route::post('/vendorLogin', 'Auth\LoginController@postLoginVendor');
+
+Route::post('/vendorRegister', 'Auth\RegisterController@createVendor');
+
+Route::get('/vendorRegister', 'Auth\LoginController@getVendorRegister');
+
+Route::get('/logout', 'Auth\LoginController@logout');
 
 Route::group(array('prefix' => 'vendor', 'namespace' => 'Vendor', 'middleware' => 'vendor'), function () {
 
